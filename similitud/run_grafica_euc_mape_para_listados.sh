75dd7542c849cbdf77a561bfd71e69d0ce27efff
CQF <cquijadafuentes@gmail.com>
2024-03-09 22:43:08 -0300

    update por si acaso.

--- similitud/run_grafica_euc_mape_para_listados.sh
+++ similitud/run_grafica_euc_mape_para_listados.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+OUTPUT_FOLDER="./listados_por_hora/resultados_graficas/"
+
 for i in APCP CAPE CONV DLWRF DSWFR PEVAP PRES SPFH TMP UGRD VGRD
 do
-	echo "\tgraficando datos"
-	python3 ../../sim_promedio_celdas.py input_test_${i}_0101.txt
-	python3 ../../sim_promedio_celdas.py input_test_${i}_0315.txt
-	echo "\tobteniendo similitudes por celda"
-	../../c_sim_temporal_series_euc input_test_${i}.txt > salida_similitud_euclidea_${i}.txt
-	../../c_sim_temporal_series_mape input_test_${i}.txt > salida_similitud_mape_${i}.txt
-	echo "\tobteniendo distribución de similitudes por celda"
-	../../c_distribucion_resultados salida_similitud_euclidea_${i}.txt > toPlot_distribucion_similitud_euclidea_${i}.txt
-	../../c_distribucion_resultados salida_similitud_mape_${i}.txt > toPlot_distribucion_similitud_mape_${i}.txt
-	cd ..
+	for h in '06' '09' '12' '15' '18' '21'
+	do
+		FILENAME="${i}_NLDAS_hour_${h}"
+		INPUT_FILE="./listados_por_hora/${FILENAME}.txt"
+		echo "\tgraficando datos ${i}_NLDAS_hour_${h}.txt"
+		python3 sim_promedio_celdas.py ${INPUT_FILE}
+		echo "\tobteniendo similitudes por celda"
+		./c_sim_temporal_series_euc ${INPUT_FILE} > ${OUTPUT_FOLDER}${FILENAME}_valores_sim_euc.txt
+		./c_sim_temporal_series_mape ${INPUT_FILE} > ${OUTPUT_FOLDER}${FILENAME}_valores_sim_mape.txt
+		echo "\tobteniendo distribución de similitudes por celda"
+		./c_distribucion_resultados ${OUTPUT_FOLDER}${FILENAME}_valores_sim_mape.txt > ${OUTPUT_FOLDER}${FILENAME}_distrib_sim_mape.txt
+		./c_distribucion_resultados ${OUTPUT_FOLDER}${FILENAME}_valores_sim_mape.txt > ${OUTPUT_FOLDER}${FILENAME}_distrib_sim_mape.txt
+	done
 done
 
 echo "FIN"
