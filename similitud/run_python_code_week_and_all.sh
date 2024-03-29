3357bb2e1e67e7ce12688c09bc32bd8219cc713f
CQF <cquijadafuentes@gmail.com>
2024-03-29 13:04:01 -0300

    update por si acaso.

---
+++ similitud/run_python_code_week_and_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "$(basename $0) <python_code>"
+	echo "Correrá <python_code> con los listados de datos semanales y luego los completos."
+	exit 1
+fi
+
+for i in APCP CAPE CONV DLWRF DSWRF PEVAP PRES SPFH TMP UGRD VGRD
+do
+	python3 ${1} ./listados_por_semana/${i}_NLDAS_week_01.txt
+done
+
+for i in APCP CAPE CONVfrac DLWRF DSWRF PEVAP PRES SPFH TMP UGRD VGRD
+do
+	python3 ${1} ${i}_NLDAS_FORA0125_H.txt
+done
+
+
+
+
