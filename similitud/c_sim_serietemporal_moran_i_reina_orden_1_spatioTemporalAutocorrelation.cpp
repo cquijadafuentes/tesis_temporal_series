b39522e88b74c8c35ddf1eda05b3fc0460c32ba3
Carlos <cquijadafuentes@gmail.com>
2024-04-22 08:50:46 -0400

    corrección en correlación espacio-temporal para que no considere series fijas.

--- similitud/c_sim_serietemporal_moran_i_reina_orden_1_spatioTemporalAutocorrelation.cpp
+++ similitud/c_sim_serietemporal_moran_i_reina_orden_1_spatioTemporalAutocorrelation.cpp
@@ -12,6 +12,37 @@
 
 using namespace std;
 
+string print_celda(int x, int y, double v){
+    string s = "";
+    s += "[" + to_string(x) + "][" + to_string(y) + "] (" + to_string(v) + ")";
+    return s;
+}
+
+string print_serie(vector<double> v){
+    string s = "";
+    for(int i=0; i<v.size(); i++){
+        s += to_string(v[i]) + " ";
+    }
+    return s;
+}
+
+string print_serie(vector<int> v){
+    string s = "";
+    for(int i=0; i<v.size(); i++){
+        s += to_string(v[i]) + " ";
+    }
+    return s;
+}
+
+double promedioST(vector<int> x){
+    unsigned long long acum = 0;
+    for(int i=0; i<x.size(); i++){
+        acum += x[i];
+    }
+    acum /= x.size();
+    return acum;
+}
+
 double ZETA(vector<int> X, vector<double> Y){
     /*
         - First CORT is calculated: 
@@ -47,39 +78,12 @@ double ZETA(vector<int> X, vector<double> Y){
     double CORT = numeradorCORT / denominadorCORT;
     double PHI = 2 / (1 + exp(2*CORT));
     double ZETA = PHI * (accVolX - accVolY);
-//    cout << numeradorCORT << "\t" << denominadorCORT << "\t" << denCORTFactorX << "\t" << denCORTFactorY<< endl;
+//    if(denCORTFactorX * denCORTFactorY == 0){
+//        cout << "Denominador en 0: " << auxDiffX << " " << auxDiffY << endl;
+//        cout << print_serie(X) << endl;
+//        cout << print_serie(Y) << endl;
+//    }
     return ZETA;
-}
-
-string print_celda(int x, int y, double v){
-    string s = "";
-    s += "[" + to_string(x) + "][" + to_string(y) + "] (" + to_string(v) + ")";
-    return s;
-}
-
-string print_serie(vector<double> v){
-    string s = "";
-    for(int i=0; i<v.size(); i++){
-        s += to_string(v[i]) + " ";
-    }
-    return s;
-}
-
-string print_serie(vector<int> v){
-    string s = "";
-    for(int i=0; i<v.size(); i++){
-        s += to_string(v[i]) + " ";
-    }
-    return s;
-}
-
-double promedioST(vector<int> x){
-    unsigned long long acum = 0;
-    for(int i=0; i<x.size(); i++){
-        acum += x[i];
-    }
-    acum /= x.size();
-    return acum;
 }
 
 int main(int argc, char const *argv[]){
@@ -128,7 +132,16 @@ int main(int argc, char const *argv[]){
         }
         stPromedio[i] = (acum / (rows*cols));
     }
-
+    //  Marcando series temporales fijas: todos sus elementos iguales
+    vector<vector<bool>> serieFija(rows, vector<bool>(cols, true));
+    for(int f=0; f<rows; f++){
+        for(int c=0; c<cols; c++){
+            for(int i=1; i<lenTempSerie; i++){
+                serieFija[f][c] = serieFija[f][c] && (temporalSeries[f][c][i] == temporalSeries[f][c][i-1]);
+            }
+        }
+    }
+    
     vector<vector<double>> matrizZetha(rows, vector<double>(cols));
     for(int i=0; i<rows; i++){
         for(int j=0; j<cols; j++){
@@ -140,34 +153,42 @@ int main(int argc, char const *argv[]){
 
     // Cálculo de Medida de utocorrelación espacio-temporal
     int sumaW = 0;
+    int cantFijas = 0;
     double numerador = 0.0;
     double denominador = 0.0;
     for(int f1 = 0; f1 < rows; f1++){
         for(int c1 = 0; c1 < cols; c1++) {
-            for(int f2=(f1-1); f2<rows && f2<=(f1+1); f2++){
-                for (int c2=(c1-1); c2<cols && c2<=(c1+1); c2++){
-                    double w = 0;
-                    double factor;
-                    if((f1 != f2 || c1 != c2) && f2 >= 0 && c2 >= 0){
-//                        cout << "\tRevisando celda: " << print_celda(f2, c2, stPromedio[f2][c2]);
-                        // Para la contigüidad tipo reina las celdas vecinas que tocan
-                        // toman un valor de 1
-                        w = 1;
-                        sumaW += w;
-                        factor = w * matrizZetha[f1][c1] * matrizZetha[f2][c2];
-//                        cout << " con w=" << w << " xi=" << diff_i_promedio << " y xj " << xxx << " y resultado " << aux <<endl;
-                        numerador += factor;
+            if(!serieFija[f1][c1]){
+                for(int f2=(f1-1); f2<rows && f2<=(f1+1); f2++){
+                    for (int c2=(c1-1); c2<cols && c2<=(c1+1); c2++){
+                        double w = 0;
+                        double factor;
+                        if(!serieFija[f2][c2] && (f1 != f2 || c1 != c2) && f2 >= 0 && c2 >= 0){
+    //                        cout << "\tRevisando celda: " << print_celda(f2, c2, stPromedio[f2][c2]);
+                            // Para la contigüidad tipo reina las celdas vecinas que tocan
+                            // toman un valor de 1
+                            w = 1;
+                            sumaW += w;
+                            factor = w * matrizZetha[f1][c1] * matrizZetha[f2][c2];
+    //                        cout << " con w=" << w << " xi=" << diff_i_promedio << " y xj " << xxx << " y resultado " << aux <<endl;
+                            numerador += factor;
+                        }
                     }
                 }
+                denominador += (matrizZetha[f1][c1] * matrizZetha[f1][c1]);
+            }else{
+                cantFijas++;
             }
-            denominador += (matrizZetha[f1][c1] * matrizZetha[f1][c1]);
         }
     }
-    cout << "totalCeldas: " << (rows*cols) << endl;
-    cout << "sumaW: " << sumaW << endl;
-    cout << "numerador: " << numerador << endl;
-    cout << "denominador: " << denominador << endl;
+//    cout << "totalCeldas: " << (rows*cols) << endl;
+//    cout << "cantFijas: " << cantFijas << endl;
+//    cout << "sumaW: " << sumaW << endl;
+//    cout << "numerador: " << numerador << endl;
+//    cout << "denominador: " << denominador << endl;
     double moran_I = (((rows*cols) + 0.0) / sumaW) * (numerador / denominador);
-    cout << argv[1] << " STA = " << moran_I << endl;
+//    cout << argv[1] << " STA = " << moran_I << endl;
+    cout << "dataset\tmoranI\ttotalCeldas\tceldasFijas\ttotalW" << endl;
+    cout << argv[1] << "\t" << moran_I << "\t" << (rows*cols) << "\t" << cantFijas << "\t" << sumaW << endl;
     return 0;
 }
