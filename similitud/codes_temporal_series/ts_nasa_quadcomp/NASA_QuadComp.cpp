5f20fd784491fad76975c523aa6292b01d1c992a
CQF <cquijadafuentes@gmail.com>
2024-08-23 18:08:55 -0400

    Correcciones en QuadComp.

--- similitud/codes_temporal_series/ts_nasa_quadcomp/NASA_QuadComp.cpp
+++ similitud/codes_temporal_series/ts_nasa_quadcomp/NASA_QuadComp.cpp
@@ -2,45 +2,83 @@
 #include "NASA_QuadComp.hpp"
 
 
-NASAQuadComp::NASAQuadComp(vector<vector<vector<int>>> tseries, int dims){
-	int rows = tseries.size();			// filas
-	int cols = tseries[0].size();		// columnas
-	int lts = tseries[0][0].size();	// length of temporal series
-	int cuadRows = rows / dims;
-	if(cuadRows*dims < rows){
-		cuadRows++;
+long long int bytes_int_vector(vector<int> x){
+	int_vector<> iv_x(x.size());
+	for(int i=0; i<x.size(); i++){
+		iv_x[i] = x[i];
 	}
-	int cuadCols = cols / dims;
-	if(cuadCols*dims < cols){
-		cuadCols++;
+	util::bit_compress(iv_x);
+	return size_in_bytes(iv_x);
+}
+
+int bytes_bit_vector(bit_vector b){
+	sd_vector<> sdb(b);
+	return size_in_bytes(b);
+}
+
+
+NASAQuadComp::NASAQuadComp(vector<vector<vector<int>>> &tseries, int dims){
+	int n_rows = tseries.size();			// filas
+	int n_cols = tseries[0].size();		// columnas
+	int n_inst = tseries[0][0].size();	// length of temporal series
+	d_quad = dims;
+	// Cantidad de filas y columnas de cuadrantes según las dimensiones dims.
+	int nQuadRows = n_rows / d_quad;
+	if(nQuadRows*d_quad < n_rows){
+		nQuadRows++;
+	}
+	int nQuadCols = n_cols / d_quad;
+	if(nQuadCols*d_quad < n_cols){
+		nQuadCols++;
 	}
 	
+	cout << "nQuadRows: " << nQuadRows << " - nQuadCols: " << nQuadCols << endl;
 
-	int_vector<> firstValue(cuadRows * cuadCols);
+	bit_vector bvQSR = bit_vector(nQuadRows * nQuadCols);	// Marcar cuadrantes con Serie de referencia
+	int cantCeldasQuad = (d_quad * d_quad) * (nQuadRows * nQuadCols);
+	bit_vector bvSF = bit_vector(cantCeldasQuad);			// Marcar las series fijas
+	bit_vector bvSR = bit_vector(cantCeldasQuad);			// Marcar la serie de referencia
+
+	vector<int> valoresFV;		// Guarda el primer valor de cada serie de referencia
+	vector<int> valoresSF;		// Guarda el valor de cada serie fija
 	int iFV = 0;
 	long long int bytesTodas = 0;
-	for(int fc=0; fc<cuadRows; fc++){		// fc = fila-cuadrante
-		for(int cc=0; cc<cuadCols; cc++){	// cc = columna-cuadrante
+	for(int fc=0; fc<nQuadRows; fc++){		// fc = fila-cuadrante
+		for(int cc=0; cc<nQuadCols; cc++){	// cc = columna-cuadrante
 			// Procesando primera series del cuadrante
-			int fref = fc*dims;
-			int cref = cc*dims;
-			int val = tseries[fref][cref][0];
-			firstValue[iFV++] = zigzag_encode(val);
-			int_vector<> ivaux(lts-1);
-			for(int k=1; k<lts; k++){
-				val = tseries[fref][cref][k] - tseries[fref][cref][k-1];
-				ivaux[k-1] = zigzag_encode(val);
-			}
-			util::bit_compress(ivaux);
-			bytesTodas += size_in_bytes(ivaux);
+			int f0c = fc*d_quad;		// Fila cero (0) del cuadrante
+			int c0c = cc*d_quad;		// Columna cero (0) del cuadrante
+			int iQuad = (fc * nQuadCols) + cc;	// Índice del cuadrante en row-major
 			// Procesando el resto de las series del cuadrante
-			for(int i=0; i<dims && fref+i < rows; i++){
-				for(int j=0; j<dims && cref+j < cols; j++){
-					if(i!=0 || j!=0){
-						// NO es la primera celda (que ya esta considerada)
-						int_vector<> ivaux2(lts);
-						for(int k=0; k<lts; k++){
-							val = tseries[fref][cref][k] - tseries[fref+i][cref+j][k];
+			vector<int> serieReferencia;
+			for(int df=0; df<d_quad && f0c+df < n_rows; df++){		// delta-filas en celdas
+				for(int dc=0; dc<d_quad && c0c+dc < n_cols; dc++){		// delta-columnas en celdas
+					int posF = f0c+df;		// Fila de la celda en evaluación
+					int posC = c0c+dc;		// Columna de la celda en evaluación
+					int iCelda = getQuadLinealPosition(posF, posC);
+					int val;
+					if(esFija(tseries[posF][posC])){
+						// La serie de tiempo es fija y no se representa
+						bvSF[iCelda] = 1;
+						valoresSF.push_back(tseries[posF][posC][0]);
+					}else if(bvQSR[iQuad] == 0){
+						// NO hay serie de referencia
+						serieReferencia = tseries[posF][posC];
+						valoresFV.push_back(tseries[posF][posC][0]);
+						int_vector<> ivaux(n_inst-1);
+						for(int k=1; k<n_inst; k++){
+							val = tseries[posF][posC][k] - tseries[posF][posC][k-1];
+							ivaux[k-1] = zigzag_encode(val);
+						}
+						util::bit_compress(ivaux);
+						bytesTodas += size_in_bytes(ivaux);
+						bvQSR[iQuad] = 1;
+						bvSR[iCelda] = 1;
+					}else{
+						// SI hay serie de referencia
+						int_vector<> ivaux2(n_inst);
+						for(int k=0; k<n_inst; k++){
+							val = serieReferencia[k] - tseries[posF][posC][k];
 							ivaux2[k] = zigzag_encode(val);
 						}
 						util::bit_compress(ivaux2);
@@ -50,11 +88,15 @@ NASAQuadComp::NASAQuadComp(vector<vector<vector<int>>> tseries, int dims){
 			}
 		}
 	}
-	util::bit_compress(firstValue);
-	bytesTodas += size_in_bytes(firstValue);
+	bytesTodas += bytes_int_vector(valoresFV);
+	bytesTodas += bytes_int_vector(valoresSF);
+	bytesTodas += bytes_bit_vector(bvQSR);
+	bytesTodas += bytes_bit_vector(bvSF);
+	bytesTodas += bytes_bit_vector(bvSR);
+
 	int megaBytes = ((bytesTodas + 0.0) / 1024) / 1024;
 
-	cout << "\t" << megaBytes << "\t" << dims << "\t" << cuadRows << "\t" << cuadCols << endl;
+	cout << megaBytes << "\t" << d_quad << "\t" << nQuadRows << "\t" << nQuadCols << endl;
 }
 
 unsigned int NASAQuadComp::zigzag_encode(int i){
@@ -63,4 +105,29 @@ unsigned int NASAQuadComp::zigzag_encode(int i){
 
 unsigned int NASAQuadComp::zigzag_decode(int i){
 	 return ((i >> 1) ^ -(i & 1));
+}
+
+bool NASAQuadComp::esFija(vector<int> serie){
+	for(int j=1; j<serie.size(); j++){
+		if(serie[j] != serie[j-1]){
+			return false;
+		}
+	}
+	return true;
+}
+
+unsigned int NASAQuadComp::getQuad(int r, int c){
+	int rquad = r / d_quad;
+	int cquad = c / d_quad;
+	int iquad = (rquad * nQuadCols) + cquad;
+	return iquad;
+}
+
+unsigned int NASAQuadComp::getQuadLinealPosition(int r, int c){
+	int iQuad = getQuad(r,c);
+	int dr = r % d_quad;
+	int dc = c % d_quad;
+	int cxQuad = d_quad * d_quad;
+	int quadLinealPos = (iQuad * cxQuad) + (dr * d_quad) + dc;
+	return quadLinealPos;
 }
--- similitud/codes_temporal_series/ts_nasa_quadcomp/NASA_QuadComp.hpp
+++ similitud/codes_temporal_series/ts_nasa_quadcomp/NASA_QuadComp.hpp
@@ -15,22 +15,31 @@ using namespace sdsl;
 //  --------------------Inicio clase NASAQuadComp-------------------
 class NASAQuadComp{
 public:
-	int n_rows;     // Filas
-	int n_cols;     // Columnas
-	int n_inst;     // Instantes
-	int dcuadl;     // Dimensión del cuadrante
-	int nCuadRows;	// Cantidad de cuadrantes en filas
-	int nCuadCols;	// Cantidad de cuadrantes en columnas
+	int n_rows;		// Filas
+	int n_cols;		// Columnas
+	int n_inst;		// Instantes
+	int d_quad;		// Dimensión del cuadrante
+	int nQuadRows;	// Cantidad de cuadrantes en filas
+	int nQuadCols;	// Cantidad de cuadrantes en columnas
 	int min_value;
 	int max_value;
 
-	int_vector<> firstValue;       //
+	int_vector<> fixedValue;		//	Primer valor de cada serie fija
+	int_vector<> refFirstValue;		//	Primer valor de las series de referencia
+	vector<int_vector<>> refs;		//	Series de Referencias
+	vector<int_vector<>> series;	//	Series que no son referencia ni fijas
+	sd_vector<> bvQuadNoFijos;		//	Cuadrantes no fijos
+	sd_vector<> bvReferencias;		//	Referencias de cuadrantes no fijos
+	sd_vector<> bvSeriesFijas;		//	Series Fijas
 
-	NASAQuadComp(vector<vector<vector<int>>>, int);
+	NASAQuadComp(vector<vector<vector<int>>>&, int);
 
 private:
 	unsigned int zigzag_encode(int);
 	unsigned int zigzag_decode(int);
+	bool esFija(vector<int>);
+	unsigned int getQuad(int, int);
+	unsigned int getQuadLinealPosition(int, int);
 };
 
 #endif
