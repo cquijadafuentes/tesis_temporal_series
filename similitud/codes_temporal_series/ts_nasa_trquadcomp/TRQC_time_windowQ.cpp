985020118757d5d04c17722c50b5111ab1c7c69a
CQF <cquijadafuentes@gmail.com>
2024-09-15 19:09:24 -0300

    tiempo en window_query.

--- similitud/codes_temporal_series/ts_nasa_trquadcomp/TRQC_time_windowQ.cpp
+++ similitud/codes_temporal_series/ts_nasa_trquadcomp/TRQC_time_windowQ.cpp
@@ -1,4 +1,5 @@
 #include "TempRaster_QuadComp.hpp"
+#include "utils_time.hpp"
 
 using namespace std;
 using namespace sdsl;
@@ -28,22 +29,35 @@ int main(int argc, char const *argv[]){
 	cout << " - nQuadRows: " << nqc.nQuadRows;
 	cout << " - nQuadCols: " << nqc.nQuadCols << endl;
 
-	cout << "Procesando archivo de consultas..." << endl;
+	cout << "Cargando consultas..." << endl;
 	ifstream qf(argv[2]);
 	int n_queries;
 	qf >> n_queries;
+	vector<vector<int>> queries(n_queries, vector<int>(6));
+	for(int q=0; q<n_queries; q++){
+		qf >> queries[q][0] >> queries[q][1] >> queries[q][2] >> queries[q][3] >> queries[q][4] >> queries[q][5];
+		// Pendiente: time
+	}
 	cout << n_queries << " consultas en el archivo " << argv[3] << endl;
 
-	int reps = stoi(argv[3]);
-	cout << "Ejecutando con " << reps << " repeticiones..." << endl;
-
-	int f0, f1, c0, c1, t0, t1;
-	for(int q=0; q<n_queries; q++){
-		qf >> f0 >> f1 >> c0 >> c1 >> t0 >> t1;
-		// Pendiente: time
-		vector<vector<vector<int>>> wq = nqc.queryWindow(f0, f1, c0, c1, t0, t1);
+	int nreps = stoi(argv[3]);
+	cout << "Ejecutando con " << nreps << " repeticiones..." << endl;
+	auto t1 = time::user::now(); // Start time
+	size_t total_num_cells = 0;
+	for(int r=0; r<nreps; r++){
+		total_num_cells = 0;
+		for(int q=0; q<n_queries; q++){
+			size_t n_cells = nqc.queryWindow(queries[q][0], queries[q][1], queries[q][2], queries[q][3], queries[q][4], queries[q][5]).size();
+			total_num_cells += n_cells;
+		}
 	}
+	auto t2 = time::user::now(); // End time
+	auto time = time::duration_cast<time::milliseconds>(t2-t1);
 
+	cout << "Time: " << time << " milliseconds. ";
+	cout << "Queries: " << nreps * queries.size() << " (" << nreps << "x" << queries.size() << ") ";
+	cout << "Nº cells = " << total_num_cells << ", us/query = " << (((double)time * 1000.0)/(double)(nreps*queries.size()));
+	cout << ", us/cell = " << (((double)time * 1000.0))/(double)total_num_cells << std::endl;
 
 	return 0;
 }
