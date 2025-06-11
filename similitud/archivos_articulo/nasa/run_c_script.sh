5e80b50e49782c52bdcb5e281d1a1650b555c004
CQF <cquijadafuentes@gmail.com>
2025-06-11 01:07:23 -0400

    datos bc, ed, eg, fib en dataset nasa.

---
+++ similitud/archivos_articulo/nasa/run_c_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "$(basename $0) <c_code> <outfile>"
+	echo "Correrá <c_code> con archivos NASA."
+	echo "SE DEBE CORRER EN EL DIRECTORIO DE LOS ARCHIVOS DE DATOS!"
+	exit 1
+fi
+
+echo "Iniciado en: $(date +'%Y/%m/%d %H:%M:%S')" > ${2}
+echo "Corriendo ${0} ${1} ${2}" >> ${2}
+
+
+for i in APCP CAPE CONVfrac DLWRF DSWRF PEVAP PRES SPFH TMP UGRD VGRD
+do
+	echo " +++++++++ Ejecutando: ./${1} ${i}_NLDAS_FORA0125_H.txt" >> ${2}
+	./${1} ${i}_NLDAS_FORA0125_H.txt >> ${2}
+done
+
+echo "Finalizado en: $(date +'%Y/%m/%d %H:%M:%S')" >> ${2}
+
