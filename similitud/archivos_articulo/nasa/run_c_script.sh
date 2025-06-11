e0145a810099d843876b2b48f44c3a74e71be003
CQF <cquijadafuentes@gmail.com>
2025-06-11 01:12:01 -0400

    update nasa script.

--- similitud/archivos_articulo/nasa/run_c_script.sh
+++ similitud/archivos_articulo/nasa/run_c_script.sh
@@ -14,7 +14,7 @@ echo "Corriendo ${0} ${1} ${2}" >> ${2}
 for i in APCP CAPE CONVfrac DLWRF DSWRF PEVAP PRES SPFH TMP UGRD VGRD
 do
 	echo " +++++++++ Ejecutando: ./${1} ${i}_NLDAS_FORA0125_H.txt" >> ${2}
-	./${1} ${i}_NLDAS_FORA0125_H.txt >> ${2}
+	${1} ${i}_NLDAS_FORA0125_H.txt >> ${2}
 done
 
 echo "Finalizado en: $(date +'%Y/%m/%d %H:%M:%S')" >> ${2}
